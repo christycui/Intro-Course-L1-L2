4dd2baa8e01a9aa6dfe5d13e825b8b3906d01147
christycui <christycui14@gmail.com>
2014-09-13 08:30:44 -0400

    made some changes

--- PRS_game.rb
+++ PRS_game.rb
@@ -1,43 +1,37 @@
 # Paper Rock Scissors
 
 def win(ans1,ans2)
-	if ans1 == ans2
-		puts "It's a tie."
-	elsif (ans1 == 'paper' and ans2 == 'rock') || (ans1  == 'rock' and ans2 == 'scissor') || (ans1 == 'scissor' and ans2 == 'paper')
-		puts "You won!"
-	else 
-		puts "The computer won!"
-	end
+  if ans1 == ans2
+    puts "It's a tie."
+  elsif (ans1 == 'paper' and ans2 == 'rock') || (ans1  == 'rock' and ans2 == 'scissor') || (ans1 == 'scissor' and ans2 == 'paper')
+    puts "You won!"
+  else 
+    puts "The computer won!"
+  end
 end
 
 ans = 'Y'
 
 until ans.upcase == 'N'
-	puts "Let's play Paper Rock Scissors!"
-	begin
-		puts "You pick first: P/R/S"
-		ans1 = gets.chomp
-	end until ['P','R','S'].include?(ans1.upcase)
-	ans2 = rand(3)
+  puts "Let's play Paper Rock Scissors!"
+  begin
+    puts "You pick first: P/R/S"
+    ans1 = gets.chomp
+  end until ['P','R','S'].include?(ans1.upcase)
+  ans2 = rand(3)
 
-	if ans2 == 0
-		ans2 = "paper"
-	elsif ans2 == 1
-		ans2 = "rock"
-	else
-		ans2 = "scissor"
-	end
+ans2 = ['paper', 'rock', 'scissors'][ans2]
 
-	if ans1.upcase == 'P'
-		ans1 = "paper"
-	elsif ans1.upcase == 'R'
-		ans1 = "rock"
-	elsif ans1.upcase == 'S'
-		ans1 = "scissor"
-	end
+  if ans1.upcase == 'P'
+    ans1 = "paper"
+  elsif ans1.upcase == 'R'
+    ans1 = "rock"
+  elsif ans1.upcase == 'S'
+    ans1 = "scissor"
+  end
 
-	puts "You picked #{ans1} and computer picked #{ans2}."
-	win(ans1,ans2)
-	puts "Try again? Y/N"
-	ans = gets.chomp
+  puts "You picked #{ans1} and computer picked #{ans2}."
+  win(ans1,ans2)
+  puts "Try again? Y/N"
+  ans = gets.chomp
 end
--- calculator.rb
+++ calculator.rb
@@ -1,46 +1,46 @@
 #This is a calculator.
 
 def operation(number1,number2, operator)
-	o = operator.upcase
-	n1 = number1.to_f
-	n2 = number2.to_f
-	if o == 'A'
-		result = n1 + n2
-	elsif o == 'B'
-		result = n1 - n2
-	elsif o == 'C'
-		result = n1 * n2
-	elsif o == 'D'
-		result = n1 / n2
-	elsif o == 'E'
-		result = n1 * n1
-	elsif o == 'F'
-		result = 1 / n1
-	elsif o == 'G'
-		result = "#{n1*100}%"
-	end
-	puts "The result is #{result}."
+  o = operator.upcase
+  n1 = number1.to_f
+  n2 = number2.to_f
+  if o == 'A'
+    result = n1 + n2
+  elsif o == 'B'
+    result = n1 - n2
+  elsif o == 'C'
+    result = n1 * n2
+  elsif o == 'D'
+    result = n1 / n2
+  elsif o == 'E'
+    result = n1 * n1
+  elsif o == 'F'
+    result = 1 / n1
+  elsif o == 'G'
+    result = "#{n1*100}%"
+  end
+  puts "The result is #{result}."
 end
 
 begin 
-	puts "Hey, I'm a calculator. Give me a number."
-	number1 = gets.chomp
+  puts "Hey, I'm a calculator. Give me a number."
+  number1 = gets.chomp
 
-	puts "What do you want me to do with it? Enter a letter.\
-			 \nA) +\nB) -\nC) *\nD) /\nE) x^2\nF) 1/x\nG) %"
-	operator = gets.chomp
+  puts "What do you want me to do with it? Enter a letter.\
+       \nA) +\nB) -\nC) *\nD) /\nE) x^2\nF) 1/x\nG) %"
+  operator = gets.chomp
 
-	if ['A', 'B', 'C', 'D'].include?(operator)
-		puts "Give me a second number."
-		number2 = gets.chomp
-		operation(number1, number2, operator)
-	else
-		operation(number1,number2 = 0,operator)
-	end
+  if ['A', 'B', 'C', 'D'].include?(operator)
+    puts "Give me a second number."
+    number2 = gets.chomp
+    operation(number1, number2, operator)
+  else
+    operation(number1,number2 = 0,operator)
+  end
 
-	puts "Wanna try again? Y / N"
-	ans = gets.chomp
-end while ans == 'Y'
+  puts "Wanna try again? Y / N"
+  ans = gets.chomp
+end while ans.upcase == 'Y'
 
 
 
