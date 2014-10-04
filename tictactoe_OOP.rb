808dbe9641ab925ab8f67f45fc03b118452da334
christycui <christycui14@gmail.com>
2014-10-04 00:43:59 -0400

    renaming and smarter strategy

--- tictactoe_OOP.rb
+++ tictactoe_OOP.rb
@@ -1,99 +1,135 @@
 # OOP-style Tic Tac Toe
 
 class Board
-  attr_accessor :a
+  attr_accessor :position
 
   def initialize
-    @a= {}
-    (1..9).each {|p| a[p] = ' '}
+    @position= {}
+    (1..9).each {|n| position[n] = ' '}
   end
 
   def draw_board
     system "clear"
     puts "     |     |    
-  #{a[1]}  |  #{a[2]}  |  #{a[3]}  
+  #{position[1]}  |  #{position[2]}  |  #{position[3]}  
      |     |     
 -----+-----+-----
      |     |     
-  #{a[4]}  |  #{a[5]}  |  #{a[6]}  
+  #{position[4]}  |  #{position[5]}  |  #{position[6]}  
      |     |     
 -----+-----+-----
      |     |     
-  #{a[7]}  |  #{a[8]}  |  #{a[9]}  
+  #{position[7]}  |  #{position[8]}  |  #{position[9]}  
      |     |     "
+  end
+
+  def position_in_row_with_two_X
+    row = Game::WIN.select {|row| position.values_at(*row).count('X') == 2 and position.values_at(*row).count(' ') == 1}.flatten
+    row.select {|n| position[n] == ' '}[0]
+  end
+
+  def position_in_row_with_two_O
+    row = Game::WIN.select {|row| position.values_at(*row).count('O') == 2 and position.values_at(*row).count(' ') == 1}.flatten
+    row.select {|n| position[n] == ' '}[0]
+  end
+end
+
+class Player
+end
+
+class User < Player
+
+  def initialize
+  end
+
+  def pick(board)
+    begin
+      puts "Choose a position to place the piece: 1 to 9"
+      user_choice = gets.chomp
+    end until board.position[user_choice.to_i] == ' '
+
+    board.position[user_choice.to_i] = 'X'
   end
 
 end
 
-class Player
+class Computer < Player
 
-  def initialize(name)
-    puts "Welcome, #{name}!"
-  end 
-
-  def user_pick(board)
-    begin
-      puts "Choose a position to place the piece: 1 to 9"
-      user_pick = gets.chomp
-    end until board.a[user_pick.to_i] == ' '
-    board.a[user_pick.to_i] = 'X'
+  def initialize
   end
 
-  def computer_pick(board)
-    computer_pick = board.a.select {|k, v| v == ' '}.keys.sample
-    board.a[computer_pick] = 'O'
+  def pick(board)
+    if board.position_in_row_with_two_X
+      choice = board.position_in_row_with_two_X
+    elsif board.position_in_row_with_two_O
+      choice = board.position_in_row_with_two_O
+    else
+      choice = board.position.select {|k, v| v == ' '}.keys.sample
+    end
+
+    board.position[choice] = 'O'
+  end
+
+end
+
+# game engine
+class Game
+
+  WIN = [[1,2,3],[3,6,9],[1,4,7],[7,8,9],[2,5,8],[4,5,6],[1,5,9],[3,5,7]]
+
+  def initialize
+  end
+
+  def display_welcome_message(name)
+    puts "Enter your name to start your Tic Tac Toe game: "
+    name = gets.chomp
+    puts "Welcome, #{name}!"
   end
 
   def check_winner(board)
-    if Game::WIN.select {|set| board.a.values_at(*set).count('X') == 3} != []
+    if WIN.select {|row| board.position.values_at(*row).count('X') == 3} != []
       "You won!!"
-    elsif Game::WIN.select {|set| board.a.values_at(*set).count('O') == 3} != []
+    elsif WIN.select {|row| board.position.values_at(*row).count('O') == 3} != []
       "You lost."
-    elsif not board.a.has_value?(' ')
+    elsif not board.position.has_value?(' ')
       "It's a tie."
     else
       nil
     end
   end
 
-end
-
-class Game
-
-  WIN = [[1,2,3],[3,6,9],[1,4,7],[7,8,9],[2,5,8],[4,5,6],[1,5,9],[3,5,7]]
-
-  def initialize
-    puts "Enter your name to start: "
-    @name = gets.chomp
-  end
-
   def play
+
+    begin
+    # set up board and players
     board = Board.new
     board.draw_board
-    user = Player.new(@name)
-    begin
-      user.user_pick(board)
-      board.draw_board
-      status = user.check_winner(board)
-      user.computer_pick(board)
-      board.draw_board
-      status = user.check_winner(board)
-    end until status
-    puts status
+    user = User.new
+    computer = Computer.new
+
+    # game begins
+      begin
+        user.pick(board)
+        computer.pick(board)
+        board.draw_board
+        status = check_winner(board)
+      end until status
+      puts status
+
+      # ask user whether to play again
+      ans = play_again?
+    end while ans == 'Y'
   end
 
   def play_again?
     begin
-      puts 'Would you like to play again?'
+      puts 'Would you like to play again? Y/N'
       ans = gets.chomp
     end until ['Y','N'].include?(ans.upcase)
-    ans
+    ans.upcase
   end
 end
 
-game = Game.new
+new_game = Game.new
+new_game.play
 
-begin
-  game.play
-  ans = game.play_again?
-end until ans.upcase == 'N'
